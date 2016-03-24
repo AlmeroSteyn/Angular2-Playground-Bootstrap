d1e025bff30b5145a50d011784e94e946d19a7c4
AlmeroSteyn <almero.steyn@gmail.com>
2016-03-24 10:54:35 +0100

    Content

--- app/components/qa-demo.component.ts
+++ app/components/qa-demo.component.ts
@@ -1,10 +1,7 @@
-import {OnInit} from "angular2/core";
-import {RouteConfig} from "angular2/router";
+import {Component} from "angular2/core";
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from "angular2/router";
 import {QaDemoList} from "./qa-demo-list.component";
 import {QaDemoDetail} from "./qa-demo-detail.component";
-import {Component} from "angular2/core";
-import {ROUTER_DIRECTIVES} from "angular2/router";
-import {ROUTER_PROVIDERS} from "angular2/router";
 import {HTTP_PROVIDERS} from "angular2/http";
 import {QaDemoContacts} from "../services/qa-demo-contacts.service";
 import {QaValidators} from "../services/qa-validators.service";
